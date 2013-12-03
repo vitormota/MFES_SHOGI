c3a004e6a65aabbde9bf1897778e804fda16938e
Pedro Costa <pedro.vendascosta@gmail.com>
2013-12-03 19:25:26 +0000

    Imprime estado atual do Board.

--- src/mfes_shogi/MFES_SHOGI.java
+++ src/mfes_shogi/MFES_SHOGI.java
@@ -6,6 +6,10 @@
 
 package mfes_shogi;
 
+import myutils.R;
+import jp.vdmtools.VDM.CGException;
+import jp.vdmtools.VDM.UTIL;
+
 /**
  *
  * @author vitor_000
@@ -16,7 +20,75 @@ public class MFES_SHOGI {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        try {
+			Board b = Board.getInstance();
+			
+			for(int y=1; y<= 9; y++ )
+			{
+				for(int x=1; x<=9; x++)
+				{
+					Cell c = b.getCell(x, y);
+					
+					if( c.piece != null ){
+						prettyPrint(c.piece.getClass().getName(), c.piece.belongTo);
+						System.out.print(" ");
+					}
+					else
+						System.out.print(" -  ");
+				}
+				System.out.println();
+			}
+		} catch (CGException e) {
+			e.printStackTrace();
+		}
     }
+    
+    private static void prettyPrint(String className, Object belongTo){
+    	switch(className){
+    	case R.BISHOP: 
+    		System.out.print("Bi");
+    		printBelongTo(belongTo);
+    		break;
+    	case R.GOLDGENERAL:
+    		System.out.print("Go");
+    		printBelongTo(belongTo);
+    		break;
+    	case R.KING:
+    		System.out.print("Ki");
+    		printBelongTo(belongTo);
+    		break;
+    	case R.KNIGHT:
+    		System.out.print("Kn");
+    		printBelongTo(belongTo);
+    		break;
+    	case R.LANCE:
+    		System.out.print("La");
+    		printBelongTo(belongTo);
+    		break;
+    	case R.PAWN:
+    		System.out.print("Pa");
+    		printBelongTo(belongTo);
+    		break;
+    	case R.ROOK:
+    		System.out.print("Ro");
+    		printBelongTo(belongTo);
+    		break;
+    	case R.SILVERGENERAL:
+    		System.out.print("Si");
+    		printBelongTo(belongTo);
+    		break;
+    	default:
+    		System.out.print("Un");
+    		printBelongTo(belongTo);
+        	break;
+    	}
+    }
+
+	private static void printBelongTo(Object belongTo) {
+		if (UTIL.equals(belongTo, new quotes.White()))
+			System.out.print("W");
+		else
+			System.out.print("B");
+	}
     
 }
---
+++ src/myutils/R.java
@@ -0,0 +1,14 @@
+package myutils;
+
+public class R {
+
+	public final static String LANCE = "mfes_shogi.Lance";
+	public final static String KNIGHT = "mfes_shogi.Knight";
+	public final static String SILVERGENERAL = "mfes_shogi.SilverGeneral";
+	public final static String BISHOP = "mfes_shogi.Bishop";
+	public final static String GOLDGENERAL = "mfes_shogi.GoldGeneral";
+	public final static String KING = "mfes_shogi.King";
+	public final static String PAWN = "mfes_shogi.Pawn";
+	public final static String ROOK = "mfes_shogi.Rook";
+	
+}
