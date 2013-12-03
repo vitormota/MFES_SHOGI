dc0d4a3890c7dd3cce318740e0420f4e845e2ce7
Vitor Mota <vitor.m.m.silva@hotmail.com>
2013-12-03 15:31:35 +0000

    Java code with fixed(??) compile time errors.

---
+++ src/mfes_shogi/MFES_SHOGI.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package mfes_shogi;
+
+/**
+ *
+ * @author vitor_000
+ */
+public class MFES_SHOGI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
