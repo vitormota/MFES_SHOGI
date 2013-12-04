0bb54016018333839b2743a021942f58738324ce
Pedro Costa <pedro.vendascosta@gmail.com>
2013-12-04 19:46:33 +0000

    Testes

--- classes/vdmloop.bat
+++ classes/vdmloop.bat
@@ -5,7 +5,17 @@ rem Assumes specification is in Word RTF files
 set S1=BoardLine.rtf
 set S2=TestBoardLine.rtf
 set S3=Cell.rtf
+set S4=TestCell.rtf
+set S5=Bishop.rtf
+set S6=GoldGeneral.rtf
+set S7=King.rtf
+set S8=Knight.rtf
+set S9=Lance.rtf
+set S10=Pawn.rtf
+set S11=Piece.rtf
+set S12=Rook.rtf
+set S13=SilverGeneral.rtf
+set S14=TestPiece.rtf
 
-
-"C:\Program Files\The VDM++ Toolbox v9.0.2\bin\vppde" -p -R vdm.tc %S1% %S2% %S3%
+"C:\Program Files\The VDM++ Toolbox v9.0.2\bin\vppde" -p -R vdm.tc %S1% %S2% %S3% %S4% %S5% %S6% %S7% %S8% %S9% %S10% %S11% %S12% %S13% %S14% 
 for /R %%f in (*.arg) do call vdmtest "%%f"
--- classes/vdmtest.bat
+++ classes/vdmtest.bat
@@ -8,9 +8,20 @@ rem short names for specification files in Word RTF Format
 set S1=BoardLine.rtf
 set S2=TestBoardLine.rtf
 set S3=Cell.rtf
+set S4=TestCell.rtf
+set S5=Bishop.rtf
+set S6=GoldGeneral.rtf
+set S7=King.rtf
+set S8=Knight.rtf
+set S9=Lance.rtf
+set S10=Pawn.rtf
+set S11=Piece.rtf
+set S12=Rook.rtf
+set S13=SilverGeneral.rtf
+set S14=TestPiece.rtf
 
 rem -- Calls the interpreter for this test case
-"C:\Program Files\The VDM++ Toolbox v9.0.2\bin\vppde" -i -D -I -P -Q -R vdm.tc -O %1.res %1 %S1% %S2% %S3%
+"C:\Program Files\The VDM++ Toolbox v9.0.2\bin\vppde" -i -D -I -P -Q -R vdm.tc -O %1.res %1 %S1% %S2% %S3% %S4% %S5% %S6% %S7% %S8% %S9% %S10% %S11% %S12% %S13% %S14% 
 
 rem -- Check for difference between result of execution and expected result.
 if EXIST %1.exp fc /w %1.res %1.exp
